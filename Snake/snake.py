34ee5ee389e215d24252f71c69a2956de29990cf
dxhw <dhsiaowecks@gmail.com>
2025-11-03 00:27:58 -0500

    Snake: fix bug with non-ai mode

--- Snake/snake.py
+++ Snake/snake.py
@@ -134,8 +134,9 @@ def move_snake(direction, snake, food_pos, portal_entry=None, portal_exit=None, 
     ):
         game_over = True
 
-    if game_over and new_head == snake[-1] or new_head == other_snake[-1]:
-        game_over = False # snake can move into the spot that the snake is moving away from
+    if game_over:
+        if new_head == snake[-1] or (len(other_snake) > 0 and new_head == other_snake[-1]):
+            game_over = False # snake can move into the spot that the snake is moving away from
 
     got_food = False
     new_portal = False
